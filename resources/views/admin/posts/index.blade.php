65f53bc363a13282057e4f5951c412c195e9832b
Chirill <chirusal2009@yahoo.com>
2017-04-24 16:49:35 +0300

    commit online

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -29,7 +29,7 @@
                 <td>{{$post->user->name}}</td>
                 <td>{{$post->category? $post->category->name : 'no catgory'}}</td>
 
-                <td><a href="">{{$post->title}}</a></td>
+                <td><a href="{{route('admin.posts.edit',$post->id)}}">{{$post->title}}</a></td>
                 <td>{{$post->body}}</td>
                 <td>{{$post->created_at}}</td>
                 <td>{{$post->updated_at}}</td>
